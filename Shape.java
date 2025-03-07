5200f39bc7a10853b85dab78e70d30ba9434dc74
sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-03-07 22:50:17 +0530

    Shape created

--- Shape.java
+++ Shape.java
@@ -1,5 +1,11 @@
 abstract class Shape {
     protected double dim_one, dim_two, dim_three;
 
+    public Shape(double dim_one, double dim_two, double dim_three) {
+        this.dim_one = dim_one;
+        this.dim_two = dim_two;
+        this.dim_three = dim_three;
+    }
+
 }
 
