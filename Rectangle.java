276442e777f18a737b98c14aa4ac90b49bc0be67
sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-03-14 17:39:56 +0530

    comments added

--- Rectangle.java
+++ Rectangle.java
@@ -1,17 +1,21 @@
 class Rectangle extends Shape {
 
+    // Constructor for Rectangle, initializes length and width
     public Rectangle(double length, double width) {
-        super(length, width, 0);
+        super(length, width, 0); // Calls parent constructor, only length and width are needed
     }
 
     @Override
     public double calculateArea() {
+        // Area of a rectangle: length * width
         return dim_one * dim_two;
     }
 
     @Override
     public double calculatePerimeter() {
+        // Perimeter of a rectangle: 2 * (length + width)
         return 2 * (dim_one + dim_two);
     }
 }
+
 
