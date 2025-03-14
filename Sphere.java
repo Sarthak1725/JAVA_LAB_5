8f21fae6ac472639530dc4b8bf5b85b4df1c89de
sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-03-14 17:34:10 +0530

    Created interface

--- Sphere.java
+++ Sphere.java
@@ -1,21 +1,27 @@
 class Sphere extends Shape implements Volume {
 
+    // Constructor for Sphere, initializes radius
     public Sphere(double radius) {
-        super(radius, 0, 0);
+        super(radius, 0, 0); // Calls parent constructor, only radius is needed
     }
 
     @Override
     public double calculateArea() {
+        // Surface area of a sphere: 4 * π * r^2
         return 4 * Math.PI * dim_one * dim_one;
     }
 
     @Override
     public double calculatePerimeter() {
+        // A sphere does not have a perimeter, so return 0
         return 0;
     }
+    
 
     @Override
     public double calculateVolume() {
+        // Volume of a sphere: (4/3) * π * r^3
         return (4.0 / 3.0) * Math.PI * Math.pow(dim_one, 3);
     }
 }
+
