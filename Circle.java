275e65080add2d2eb6c974cf8cfed856768b4a23
sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-03-14 17:36:08 +0530

    code

--- Circle.java
+++ Circle.java
@@ -1,16 +1,20 @@
 class Circle extends Shape {
 
+    // Constructor for Circle, initializes the radius
     public Circle(double radius) {
-        super(radius, 0, 0);
+        super(radius, 0, 0); // Calls parent constructor, only radius is needed
     }
 
     @Override
     public double calculateArea() {
+        // Area of a circle: π * r^2
         return Math.PI * dim_one * dim_one;
     }
 
     @Override
     public double calculatePerimeter() {
+        // Circumference of a circle: 2 * π * r
         return 2 * Math.PI * dim_one;
     }
 }
+
