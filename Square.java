694d57d1c3a2d779a073da22566c7bf379743e89
sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-03-14 17:37:58 +0530

    comments added

--- Square.java
+++ Square.java
@@ -1,17 +1,21 @@
 class Square extends Shape {
 
+    // Constructor for Square, initializes side length
     public Square(double side) {
-        super(side, 0, 0);
+        super(side, 0, 0); // Calls parent constructor, only side length is needed
     }
 
     @Override
     public double calculateArea() {
+        // Area of a square: side^2
         return dim_one * dim_one;
     }
 
     @Override
     public double calculatePerimeter() {
+        // Perimeter of a square: 4 * side
         return 4 * dim_one;
     }
 }
+
 
