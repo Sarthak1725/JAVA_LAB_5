436685425a38741a86fd17ead469e681660745d3
sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-03-14 17:33:02 +0530

    Comments added

--- Sphere.java
+++ Sphere.java
@@ -1,3 +1,21 @@
-public class Sphere {
-    
+class Sphere extends Shape implements Volume {
+
+    public Sphere(double radius) {
+        super(radius, 0, 0);
+    }
+
+    @Override
+    public double calculateArea() {
+        return 4 * Math.PI * dim_one * dim_one;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 0;
+    }
+
+    @Override
+    public double calculateVolume() {
+        return (4.0 / 3.0) * Math.PI * Math.pow(dim_one, 3);
+    }
 }
